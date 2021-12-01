29b7003b3d88ece61b02c4656648a0dbac98b6ff
jay graves <jaywgraves@gmail.com>
2021-12-01 15:08:43 -0600

    switch p5 to instance mode  closes #13

--- src/static/js/gen.js
+++ src/static/js/gen.js
@@ -1,184 +1,190 @@
-var cnv;
-var gif;
-let idx = 0;
-let interval_id = 0;
-let font;
 
-function get_sin(idx, a, k, h){
-  x = ((2*Math.PI)/60)*idx;
-  return a * Math.sin(x+h) + k;
-}
+var sketch = function(p) {
+  p.cnv = null;
+  p.gif = null;
+  p.idx = 0;
+  p.interval_id = 0;
+  p.font = null;
 
-function get_tri(idx, a, k, h){
-  x = ((2*Math.PI)/60)*idx;
-  return (2*a)/Math.PI * (Math.asin(Math.sin(x + h))) + k;
-}
-
-function preload(){
-  font = loadFont('static/fonts/Ubuntu-B.ttf');
-}
-
-function setup() {
-  noLoop();
-  cnv = createCanvas(200, 200);
-  cnv.parent('gifcanvas');
-  textFont(font);
-  angleMode(DEGREES);
-  setupGIF();
-  setindex();
-}
-
-function get_params() {
-  params = {};
-  // text
-  params.fcolor = select('#fcolor').elt.value
-  params.scolor = select('#scolor').elt.value
-  params.txt = select('#inptext').elt.value
-  params.ssize= select('#ssize').elt.value
-  params.txtstart = parseInt(select('#txtstart').elt.value)
-  params.txtamp = parseInt(select('#txtamp').elt.value)
-  params.rotamp = parseInt(select('#rotamp').elt.value)
-  // background
-  params.colorcnt = parseInt(select('#colorcnt').elt.value)
-  params.sectioncnt = parseInt(select('#sectioncnt').elt.value)
-  params.angles = []
-  params.colors = []
-  for (i=0; i < params.colorcnt; i++) {
-    params.angles.push(parseInt(select('#angle' + (i+1)).elt.value))
-    params.colors.push(select('#color' + (i+1)).elt.value)
-  }
-  return params
-}
-
-function start() {
-  preview = true;
-  function doit2(){
-    doit(null, preview)
-  }
-  interval_id = setInterval(doit2, 40);
-  interval_elem = select('#interval')
-  interval_elem.elt.value = interval_id
-}
-
-function stop() {
-  interval_elem = select('#interval')
-  interval_id = interval_elem.elt.value
-  clearInterval(interval_id)
-}
-
-function saveemoji() {
-  stop()
-  idx = 0
-  params = get_params();
-  preview = false;
-  function doit2(){
-    doit(params, preview)
+  p.get_sin = function(idx, a, k, h){
+    x = ((2*Math.PI)/60)*idx;
+    return a * Math.sin(x+h) + k;
   }
 
-  interval_id = setInterval(doit2, 40);
-  interval_elem = select('#interval')
-  interval_elem.elt.value = interval_id
-}
-
-function setindex(x) {
-  stop()
-  if (typeof x == 'undefined') {
-    idx = parseInt(document.querySelector('#frameidx').value)
-  } else {
-    idx += x
+  p.get_tri = function(idx, a, k, h){
+    x = ((2*Math.PI)/60)*idx;
+    return (2*a)/Math.PI * (Math.asin(Math.sin(x + h))) + k;
   }
-  params = get_params();
-  doit(params, true)
-}
 
-
-function doit(params, preview) {
-  if (!params) {
-    params = get_params()
+  p.preload = function(){
+    p.font = p.loadFont('static/fonts/Ubuntu-B.ttf');
   }
-  clear();
-  // background
-  drawbackground(width / 2, height / 2, idx*6, params)
 
-  // text
-  drawtext(width / 2, height / 2, idx, params)
-
-  if (!preview) {
-  gif.addFrame(cnv.elt, {
-            delay: 40,
-            copy: true
-        });
+  p.setup = function() {
+    p.noLoop();
+    p.cnv = p.createCanvas(200, 200);
+    p.cnv.parent('gifcanvas');
+    p.textFont(p.font);
+    p.angleMode(p.DEGREES);
+    p.setupGIF();
+    p.setindex();
   }
-  idx += 1;
-  if (idx >= 60 ) {
-    idx = 0;
+
+  p.get_params = function() {
+    params = {};
+    // text
+    params.fcolor = p.select('#fcolor').elt.value
+    params.scolor = p.select('#scolor').elt.value
+    params.txt = p.select('#inptext').elt.value
+    params.ssize= p.select('#ssize').elt.value
+    params.txtstart = parseInt(p.select('#txtstart').elt.value)
+    params.txtamp = parseInt(p.select('#txtamp').elt.value)
+    params.rotamp = parseInt(p.select('#rotamp').elt.value)
+    // background
+    params.colorcnt = parseInt(p.select('#colorcnt').elt.value)
+    params.sectioncnt = parseInt(p.select('#sectioncnt').elt.value)
+    params.angles = []
+    params.colors = []
+    for (i=0; i < params.colorcnt; i++) {
+      params.angles.push(parseInt(p.select('#angle' + (i+1)).elt.value))
+      params.colors.push(p.select('#color' + (i+1)).elt.value)
+    }
+    return params
+  }
+
+  p.start = function() {
+    preview = true;
+    p.doit2 =function(){
+      p.doit(null, preview)
+    }
+    p.interval_id = setInterval(p.doit2, 40);
+    p.interval_elem = p.select('#interval')
+    p.interval_elem.elt.value = p.interval_id
+  }
+
+  p.stop = function() {
+    p.interval_elem = p.select('#interval')
+    p.interval_id = p.interval_elem.elt.value
+    clearInterval(p.interval_id)
+  }
+
+  p.saveemoji = function() {
+    p.stop()
+    p.idx = 0
+    p.params = p.get_params();
+    preview = false;
+    p.doit2 = function(){
+      p.doit(params, preview)
+    }
+
+    p.interval_id = setInterval(p.doit2, 40);
+    p.interval_elem = p.select('#interval')
+    p.interval_elem.elt.value = p.interval_id
+  }
+
+  p.setindex = function(x) {
+    p.stop()
+    if (typeof x == 'undefined') {
+      idx = parseInt(document.querySelector('#frameidx').value)
+    } else {
+      idx += x
+    }
+    p.params = p.get_params();
+    p.doit(p.params, true)
+  }
+
+
+  p.doit = function(params, preview) {
+    if (!params) {
+      params = p.get_params()
+    }
+    p.clear();
+    // background
+    p.drawbackground(p.width / 2, p.height / 2, p.idx*6, params)
+
+    // text
+    p.drawtext(p.width / 2, p.height / 2, p.idx, params)
+
     if (!preview) {
-      clearInterval(interval_id)
-      gif.render();
+    p.gif.addFrame(p.cnv.elt, {
+              delay: 40,
+              copy: true
+          });
     }
+    p.idx += 1;
+    if (p.idx >= 60 ) {
+      p.idx = 0;
+      if (!preview) {
+        clearInterval(p.interval_id)
+        p.gif.render();
+      }
+    }
+    document.querySelector("#frameidx").value = p.idx;
   }
-  document.querySelector("#frameidx").value = idx;
+
+  p.drawtext = function(x, y, idx, params){
+    if (!params.txt) { return ;}
+    scolor = p.color(params.scolor)
+    //scolor.setAlpha(params.scoloralpha)
+    fcolor = p.color(params.fcolor)
+    //fcolor.setAlpha(params.fcoloralpha)
+    p.textSize(p.get_tri(idx, params.txtamp, params.txtstart, 0));
+    p.textAlign(p.CENTER, p.CENTER);
+    if (params.ssize > 0) {
+      p.stroke(scolor);
+      p.strokeWeight(params.ssize);
+    } else {
+      p.noStroke();
+    };
+    p.fill(fcolor);
+    bbox = p.font.textBounds(params.txt, x, y)
+    p.push()
+    dx = bbox.x + (bbox.w / 2)
+    dy = bbox.y + (bbox.h / 2)
+    p.translate(dx , dy)
+    p.rotate(p.get_tri(idx, params.rotamp, 0, 0))
+    p.translate(-dx , -dy)
+    //p.rect(bbox.x, bbox.y, bbox.w, bbox.h)  //debug bounding box
+    p.text(params.txt, x, y);
+    p.pop()
+  }
+
+  p.drawbackground = function(x, y, startangle, params){
+    // startangle = idx * 6 which is the starting spot of the arc
+    p.noStroke();
+    params.arcsize = 300;
+    angle = 0;
+    for (let i = 0; i < params.sectioncnt; i++) {
+        p.fill(params.colors[i % params.colors.length]);
+        arcbeg = startangle + angle
+        arcend = arcbeg + params.angles[i % params.angles.length]
+        p.arc(x, y, params.arcsize, params.arcsize, arcbeg, arcend);
+        angle += params.angles[i % params.angles.length];
+      }
+
+    p.fill(params.colors[0]);
+    p.ellipse(x, y, 50, 50);
+  }
+
+  p.setupGIF = function() {
+    p.gif = new GIF({
+        workers: 5,
+        quality: 20,
+        workerScript: "static/js/gif.worker.js"
+    });
+    p.gif.on('finished', function(blob) {
+        window.open(URL.createObjectURL(blob));
+        p.gif.abort();
+        p.gif.frames = [];
+        delete p.gif.imageParts;
+        delete p.gif.finishedFrames;
+        delete p.gif.nextFrame;
+    });
+  }
 }
 
-function drawtext(x, y, idx, params){
-  if (!params.txt) { return ;}
-  scolor = color(params.scolor)
-  //scolor.setAlpha(params.scoloralpha)
-  fcolor = color(params.fcolor)
-  //fcolor.setAlpha(params.fcoloralpha)
-  textSize(get_tri(idx, params.txtamp, params.txtstart, 0));
-  textAlign(CENTER, CENTER);
-  if (params.ssize > 0) {
-    stroke(scolor);
-    strokeWeight(params.ssize);
-  } else {
-    noStroke();
-  };
-  fill(fcolor);
-  bbox = font.textBounds(params.txt, x, y)
-  push()
-  dx = bbox.x + (bbox.w / 2)
-  dy = bbox.y + (bbox.h / 2)
-  translate(dx , dy)
-  rotate(get_tri(idx, params.rotamp, 0, 0))
-  translate(-dx , -dy)
-  //rect(bbox.x, bbox.y, bbox.w, bbox.h)  //debug bounding box
-  text(params.txt, x, y);
-  pop()
-}
+var sf = new p5(sketch);
 
-function drawbackground(x, y, startangle, params){
-  // startangle = idx * 6 which is the starting spot of the arc
-  noStroke();
-  params.arcsize = 300;
-  angle = 0;
-  for (let i = 0; i < params.sectioncnt; i++) {
-      fill(params.colors[i % params.colors.length]);
-      arcbeg = startangle + angle
-      arcend = arcbeg + params.angles[i % params.angles.length]
-      arc(x, y, params.arcsize, params.arcsize, arcbeg, arcend);
-      angle += params.angles[i % params.angles.length];
-    }
-
-  fill(params.colors[0]);
-  ellipse(x, y, 50, 50);
-}
-
-function setupGIF() {
-  gif = new GIF({
-      workers: 5,
-      quality: 20,
-      workerScript: "static/js/gif.worker.js"
-  });
-  gif.on('finished', function(blob) {
-      window.open(URL.createObjectURL(blob));
-      gif.abort();
-      gif.frames = [];
-      delete gif.imageParts;
-      delete gif.finishedFrames;
-      delete gif.nextFrame;
-  });
-}
 colorcfg = {
   1 : {default:1, enabled: [1]},
   2 : {default:8, enabled: [2,4,6,8,10,12]},
@@ -192,8 +198,8 @@ colorcfg = {
   12 : {default:12, enabled: [12]}
 }
 
-function resetUI() {
-  document.querySelector("#frameidx").value = idx;
+resetUI = function() {
+  //document.querySelector("#frameidx").value = idx;
   var colorcnt = parseInt(document.querySelector("#colorcnt").value);
   var last_colorcnt_elem = document.querySelector("#last_colorcnt");
   var last_colorcnt = parseInt(last_colorcnt_elem.value);
